7b2c5250969bb92c7ddbfba039985e000b816a39
yufanren <yr975@nyu.edu>
2021-01-17 20:38:20 -0500

    fixed name

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from flaskblog import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,14 +2,17 @@ import os
 
 #run python app.py instead of running from pycharm
 class Config:
-    SECRET_KEY = '31c5abf54096e0d96d440567e38d4209'
-    #SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgres://brrojcdqutmoht:4231a5aaf978bf068247e6ae879ace4dc82443353746209fa55ed25ebc6ea047@ec2-34-202-5-87.compute-1.amazonaws.com:5432/d2bid53o4i3ftb'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    ENV = 'DEP'
     MAIL_SERVER = 'smtp.mailtrap.io'
     MAIL_PORT = 2525
     MAIL_USE_TLS = True
-    MAIL_USERNAME = '93ec69c7272171'
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = 'ff90d7da0930ad'
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    if ENV == 'DEV':
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    else:
+        SECRET_KEY = '31c5abf54096e0d96d440567e38d4209'
+        SQLALCHEMY_DATABASE_URI = 'postgres://brrojcdqutmoht:4231a5aaf978bf068247e6ae879ace4dc82443353746209fa55ed25ebc6ea047@ec2-34-202-5-87.compute-1.amazonaws.com:5432/d2bid53o4i3ftb'
+        MAIL_USERNAME = '93ec69c7272171'
+        MAIL_PASSWORD = 'ff90d7da0930ad'
